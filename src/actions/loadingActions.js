788f4d2fa4cbcc90a95555831e1f55fcabd0b4a3
mgonciarz <mikolaj.gonciarz@dreamlab.pl>
2019-05-19 17:45:00 +0200

    removed axios

--- src/actions/loadingActions.js
+++ src/actions/loadingActions.js
@@ -1,6 +1,5 @@
 import { actionTypes } from '../helpers/actionTypes.js';
 import { BEER_LIST, RANDOM_BEER } from '../helpers/beerEndpoints';
-import axios from 'axios';
 
 const { LOADING, SUCCESS, FAIL, GET_BEER_LIST, GET_ACTIVE_BEER } = actionTypes;
 
@@ -12,33 +11,35 @@ const loadSuccess = actionCreator(SUCCESS);
 const loadFail = actionCreator(FAIL);
 
 export const getBeerList = () => async dispatch => {
-  try {
-    dispatch(loadStart());
-    const res = await axios.get(BEER_LIST);
+	try {
+		dispatch(loadStart());
+		const res = await fetch(BEER_LIST);
+		const data = await res.json();
 
-    dispatch(loadSuccess());
-    dispatch({
-      type: GET_BEER_LIST,
-      payload: res.data
-    });
-  } catch (e) {
-    dispatch(loadFail(e));
-  }
-}
+		dispatch(loadSuccess());
+		dispatch({
+		  type: GET_BEER_LIST,
+		  payload: data
+		});
+	} catch (e) {
+		dispatch(loadFail(e));
+	}
+};
 
 export const getRandomBeer = () => async dispatch => {
-  try {
-    dispatch(loadStart());
-    const res = await axios.get(RANDOM_BEER);
+	try {
+		dispatch(loadStart());
+		const res = await fetch(RANDOM_BEER);
+		const data = await res.json();
 
-    dispatch(loadSuccess());
-    dispatch({
-      type: GET_ACTIVE_BEER,
-      payload: res.data[0]
-    });
-  } catch (e) {
-    dispatch(loadFail(e));
-  }
-}
+		dispatch(loadSuccess());
+		dispatch({
+			type: GET_ACTIVE_BEER,
+			payload: data[0]
+		});
+	} catch (e) {
+		dispatch(loadFail(e));
+	}
+};
 
 export const setActiveBeer = actionCreator(GET_ACTIVE_BEER);
